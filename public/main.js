849d99b7487dce03b5d101cf2f4ccc59d8e9c7b2
Arild Wanvik Tvergrov <arildt@mesan.no>
2014-08-25 23:14:02 +0200

    Solve Angular assignment 3.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,39 @@
 (function () {
-  // angular available here...
+  var newsletterApp = angular.module('newsletterApp', []);
+
+  newsletterApp.controller('NewsletterFormController', function ($scope, $rootScope, newsletterService) {
+    $scope.subscribeUser = function () {
+      var user = {
+        email: $scope.email,
+        firstName: $scope.firstName,
+        lastName: $scope.lastName
+      };
+
+      newsletterService.addUser(user);
+
+      $rootScope.$broadcast('userSubscribed');
+    };
+  });
+
+  newsletterApp.controller('NewsletterTableController', function ($scope, $rootScope, newsletterService) {
+
+    function refreshUsers() {
+      $scope.users = newsletterService.getUsers();
+    }
+
+    $rootScope.$on('userSubscribed', refreshUsers);
+  });
+
+  newsletterApp.service('newsletterService', function () {
+    var users = [
+    ];
+
+    this.getUsers = function () {
+      return users;
+    };
+
+    this.addUser = function (user) {
+      users.push(user);
+    };
+  });
 })();
