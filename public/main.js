7b89c1a5ff09cb6d48d68362e36bcba617c9b0b9
Arild Wanvik Tvergrov <arildt@mesan.no>
2014-08-25 22:20:19 +0200

    Solve Angular assignment 1.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,44 @@
 (function () {
-  // angular available here...
+  var superheroesApp = angular.module('superheroesApp', []);
+
+  superheroesApp.controller('SuperheroesController', function ($scope, superheroesService) {
+    $scope.chosenSuperhero = null;
+
+    $scope.chooseRandomSuperhero = function () {
+      $scope.chosenSuperhero = superheroesService.getRandomSuperhero();
+    };
+  });
+
+  superheroesApp.service('superheroesService', function () {
+    var superheroes = [
+      "Batman",
+      "Superman",
+      "Flash",
+      "Green Lantern",
+      "Green Arrow",
+      "Wonder Woman",
+      "Martian Manhunter",
+      "Robin/Nightwing",
+      "Blue Beetle",
+      "Black Canary",
+      "Spider Man",
+      "Captain America",
+      "Iron Man",
+      "Thor",
+      "Hulk",
+      "Wolverine",
+      "Daredevil",
+      "Hawkeye",
+      "Cyclops",
+      "Silver Surfer"
+    ];
+
+    function getRandomIndex() {
+      return Math.floor(Math.random() * superheroes.length);
+    }
+
+    this.getRandomSuperhero = function () {
+      return superheroes[getRandomIndex()];
+    };
+  });
 })();
