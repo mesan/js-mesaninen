b0a92d92a160ce945d36be88d21e5851fff18b0b
Arild Wanvik Tvergrov <arildt@mesan.no>
2014-08-26 00:09:54 +0200

    Solve Knockout assignment.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,25 @@
 (function () {
-  // ko available here...
+  var words = {
+    'bord': 'table',
+    'prosjekt': 'project',
+    'ansikt': 'face',
+    'jeg': 'I',
+    'samma': 'whatever',
+    'bacon': 'bacon'
+  };
+
+  var viewModel = {
+    word: ko.observable(),
+    translatedWord: ko.observable(),
+    noResults: ko.observable(false),
+    translate: function () {
+      var translatedWord = words[this.word()];
+
+      this.noResults(translatedWord === undefined);
+
+      this.translatedWord(translatedWord);
+    }
+  };
+
+  ko.applyBindings(viewModel);
 })();
