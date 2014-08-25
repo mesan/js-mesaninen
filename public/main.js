a91f2b97c09db7042095347ea4806472626b79ca
Arild Wanvik Tvergrov <arildt@mesan.no>
2014-08-25 22:39:09 +0200

    Solve Angular assignment 2.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,42 @@
 (function () {
-  // angular available here...
+  var gameSearchApp = angular.module('gameSearchApp', []);
+
+  gameSearchApp.controller('GameSearchController', function ($scope, gameService) {
+    $scope.games = gameService.getGames();
+  });
+
+  gameSearchApp.service('gameService', function () {
+    this.getGames = function () {
+      return [
+        {
+          title: 'FIFA 15',
+          released: 2015
+        },
+        {
+          title: 'Final Fantasy IX',
+          released: 2000
+        },
+        {
+          title: 'Grand Theft Auto V',
+          released: 2013
+        },
+        {
+          title: 'Pacman',
+          released: 1980
+        },
+        {
+          title: 'The Elder Scrolls V: Skyrim',
+          released: 2011
+        },
+        {
+          title: 'The Last of Us',
+          released: 2014
+        },
+        {
+          title: 'Tetris',
+          released: 1984
+        }
+      ];
+    };
+  });
 })();
