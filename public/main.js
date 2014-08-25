00b4f4e88c35be6d89d35f3536703243747da2a2
Arild Wanvik Tvergrov <arildt@mesan.no>
2014-08-25 23:50:59 +0200

    Solve Angular assignment 4.

--- public/main.js
+++ public/main.js
@@ -1,3 +1,16 @@
 (function () {
-  // angular available here...
+  var wikipediaApp = angular.module('wikipediaApp', []);
+
+  wikipediaApp.directive('wikipedia', function () {
+    return {
+      restrict: 'E',
+      scope: {
+        lang: '@',
+        what: '@'
+      },
+      template: '<a ng-href="http://{{lang}}.wikipedia.org/wiki/{{what}}" target="_blank">' +
+        '{{what}} on Wikipedia' +
+        '</a>'
+    };
+  });
 })();
